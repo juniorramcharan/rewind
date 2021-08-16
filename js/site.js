fea8bebdc63ad2a103693a0b983fcc207e8cf367
Charles Ramcharan <charlesramcharan@yahoo.com>
2021-08-16 11:05:51 -0400

    final revers string project

--- js/site.js
+++ js/site.js
@@ -1,11 +1,28 @@
 //get values from the interface
-function reverseString(stringValue){
-
+function getTheReverse(stringValue){
+    //get String Value.
     let stringValues = document.getElementById("stringValue").value;
+                        document.getElementById("alert").classList.add("invisible");
+    //reverse the string value
+  let reverseValue =  doTheRevers(stringValues);
+
+    //display the value
+    displayTheReverse(reverseValue);
+    
+}
+function doTheRevers(valueToReverse){
     let result =[];
-   for (let index = stringValues.length-1; index >= 0;  index--) {
-      
-       result += stringValues[index];
-    }
-    return result;
+    for (let index = valueToReverse.length-1; index >= 0;  index--) {
+       
+        result += valueToReverse[index];
+     }
+     return result;
+
+}
+
+function displayTheReverse(myValue){
+
+    document.getElementById('result').innerHTML =`Your reverse string is: ${myValue}`;
+    document.getElementById("alert").classList.remove("invisible");
+
 }
